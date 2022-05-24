ea9c36cef2ffceda68695aa906a5f603e31fbf14
dewansh-shukla <dewanshshukla2002@gmail.com>
2022-05-24 18:34:27 +0530

    config

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+require("dotenv").config()
+const cors = require("cors")
+const path = require("path")
+const PORT = process.env.PORT || 4000
 
+const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json(), urlencodedParser)
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
